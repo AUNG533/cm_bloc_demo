f96028cf5e9b7b01d261fe7a9835836ea7321119
aungkyawlin <aungkyawlinmon@gmail.com>
2022-04-18 17:57:54 +0700

    Start and setup project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:cm_bloc_demo/src/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+
