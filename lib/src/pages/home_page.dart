49f30fccf45ffbe51f8f5d016db7b750650ccc3e
aungkyawlin <aungkyawlinmon@gmail.com>
2022-04-18 20:41:55 +0700

    setup project

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -12,12 +12,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +23,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'CounterA',
             ),
             Text(
               '$_counter',
@@ -38,10 +32,21 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {},
+            tooltip: 'Reset',
+            child: const Icon(Icons.restore),
+          ),
+          const SizedBox(height: 10,),
+          FloatingActionButton(
+            onPressed: () {},
+            tooltip: 'Add',
+            child: const Icon(Icons.add),
+          )
+        ],
       ),
     );
   }
